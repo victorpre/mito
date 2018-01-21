9103b3dd09cde603c2898994b6664a7d6db3b4ee
Victor Presumido <victor.presumido@gmail.com>
2018-01-21 20:59:54 -0200

    Add amnesia to project
    - Config multiple nodes for dev environemtn

--- api/config/dev.exs
+++ api/config/dev.exs
@@ -20,7 +20,7 @@ use Mix.Config
 # configured to run both http and https servers on
 # different ports.
 config :mito, Mito.Endpoint,
-  http: [port: 4000],
+  http: [port: System.get_env("PORT") || 4000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
