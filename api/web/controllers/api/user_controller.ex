84d8208387f15f29d771cb5458277bb972559998
Victor Presumido <victor.presumido@gmail.com>
2018-02-12 18:24:09 -0200

    Create ejabberd user after creating user in ecto

--- api/web/controllers/api/user_controller.ex
+++ api/web/controllers/api/user_controller.ex
@@ -10,9 +10,9 @@ defmodule Mito.UserController do
 
   def create(conn, %{"user" => user_params}) do
     changeset = User.registration_changeset(%User{}, user_params)
-
     case Repo.insert(changeset) do
       {:ok, user} ->
+        User.create_ejabberd_user(user, conn.host)
         conn
         |> put_status(:created)
         |> put_resp_header("location", user_path(conn, :show, user))
