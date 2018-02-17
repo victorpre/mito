776e960cd96107fd89d15eeea51261a4623fe3f4
Victor Presumido <victor.presumido@gmail.com>
2018-02-17 14:50:19 -0200

    Configure Guardian for test env

--- api/config/test.exs
+++ api/config/test.exs
@@ -22,3 +22,6 @@ config :ejabberd,
   file: "config/ejabberd.test.yml",
   log_path: 'logs/ejabberd.test.log'
 
+  config :mito, Mito.Auth.Guardian,
+  secret_key: "00hX+rX7pusXXsChSB6i+4xVYeIFaErFDlpLa3BbQ7pmObXYQdb3OBArTQ6rR2Dj"
+
