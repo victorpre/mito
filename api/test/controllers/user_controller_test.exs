6762e664c3efb071110529d0f711d3134aed7aad
Victor Presumido <victor.presumido@gmail.com>
2018-02-17 14:50:48 -0200

    Refactor user controller tests

--- api/test/controllers/user_controller_test.exs
+++ api/test/controllers/user_controller_test.exs
@@ -16,27 +16,6 @@ defmodule Mito.UserControllerTest do
     end
   end
 
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, user_path(conn, :index)
-    assert json_response(conn, 200)["data"] == []
-  end
-
-  test "shows chosen user", %{conn: conn} do
-    user = insert(:user)
-    conn = get conn, user_path(conn, :show, user)
-    assert json_response(conn, 200)["data"] == %{"id" => user.id,
-      "username" => user.username,
-      "email" => user.email,
-      "name" => user.name
-    }
-  end
-
-  test "renders page not found when id is nonexistent", %{conn: conn} do
-    assert_error_sent 404, fn ->
-      get conn, user_path(conn, :show, -1)
-    end
-  end
-
   test "creates user when data is valid", %{conn: conn} do
     user_params = params_for(:user, @valid_attrs)
     conn = post conn, user_path(conn, :create), user: user_params
@@ -64,34 +43,5 @@ defmodule Mito.UserControllerTest do
     conn = post conn, user_path(conn, :create), user: @invalid_attrs
 
     assert json_response(conn, 422)["errors"] != %{}
-  end
-
-  test "updates chosen user when data is valid", %{conn: conn} do
-    user = insert(:user)
-    user_params = params_for(:user, @valid_attrs)
-    conn = put conn, user_path(conn, :update, user), user: user_params
-
-    assert Repo.get_by(User, username: user_params.username)
-  end
-
-  test "renders chosen user when data is valid", %{conn: conn} do
-    user = insert(:user)
-    user_params = params_for(:user, @valid_attrs)
-    conn = put conn, user_path(conn, :update, user), user: user_params
-    assert json_response(conn, 200)["data"]["id"]
-  end
-
-
-  test "does not update chosen user and renders errors when data is invalid", %{conn: conn} do
-    user = insert(:user)
-    conn = put conn, user_path(conn, :update, user), user: @invalid_attrs
-    assert json_response(conn, 422)["errors"] != %{}
-  end
-
-  test "deletes chosen user", %{conn: conn} do
-    user = insert(:user)
-    conn = delete conn, user_path(conn, :delete, user)
-    assert response(conn, 204)
-    refute Repo.get(User, user.id)
   end
 end
