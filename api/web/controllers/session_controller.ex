63b27a452c91c2e8f7faeac24f135bbc17233a80
Victor Presumido <victor.presumido@gmail.com>
2018-02-19 00:54:31 -0300

    Implement endpoint for User token refresh
    This commit adds an endpoint on the backend for the user to be able to refresh his
    access token
    
    `POST api/sessions/refresh`

--- api/web/controllers/session_controller.ex
+++ api/web/controllers/session_controller.ex
@@ -10,4 +10,24 @@ defmodule Mito.SessionController do
       {:error, msg} -> render(conn, Mito.ChangesetView, "error.json", changeset: msg)
     end
   end
+
+  def refresh(conn, _params) do
+    {:ok, user} =
+      Auth.Guardian.Plug.current_claims(conn)
+      |> Auth.Guardian.resource_from_claims()
+
+    token = Auth.Guardian.Plug.current_token(conn)
+
+    case Auth.Guardian.refresh(token) do
+      {:ok, _old_stuff, {new_token, new_claims}} ->
+        conn
+        |> put_status(:ok)
+        |> render(Mito.UserView, "login.json", user: user, token: new_token)
+
+      {:error, _reason} ->
+        conn
+        |> put_status(:unauthorized)
+        |> render(Mito.ErrorView, "forbidden.json", error: "Not authenticated")
+    end
+  end
 end
