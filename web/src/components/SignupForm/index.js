61d325bd753c230eb2b37af9ee604ffe93b4bd97
Victor Presumido <victor.presumido@gmail.com>
2018-02-18 22:56:59 -0300

    Add signup form and container

---
+++ web/src/components/SignupForm/index.js
@@ -0,0 +1,110 @@
+// @flow
+import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import {Card, Col, Row, Button} from 'react-materialize'
+import Input from '../Input';
+
+type Props = {
+  onSubmit: () => void,
+  submitting: boolean,
+  handleSubmit: () => void,
+}
+
+class SignupForm extends Component {
+  props: Props
+
+  handleSubmit = data => this.props.onSubmit(data);
+
+  render() {
+    const { handleSubmit, submitting } = this.props;
+
+    return (
+      <Row>
+        <Col m={4} s={12} offset="m4">
+          <Card title='Create and account'>
+            <form
+              onSubmit={handleSubmit(this.handleSubmit)}
+            >
+              <Row>
+                <Field
+                  label="Full Name"
+                  name="name"
+                  type="text"
+                  component={Input}
+                />
+              </Row>
+              <Row>
+                <Field
+                  label="Username"
+                  name="username"
+                  type="text"
+                  component={Input}
+                />
+              </Row>
+              <Row>
+                <Field
+                  name="email"
+                  label="Email"
+                  type="email"
+                  component={Input}
+                />
+              </Row>
+              <Row>
+                <Field
+                  name="password"
+                  label="Password"
+                  type="password"
+                  component={Input}
+                />
+              </Row>
+              <Row>
+                <Col offset="m9">
+                  <Button
+                    waves="light"
+                    type="submit"
+                    disabled={submitting}
+                  >
+                    {submitting ? 'Submitting...' : 'Sign up'}
+                  </Button>
+                </Col>
+              </Row>
+              <hr style={{ margin: '2rem 0' }} />
+              <Row
+                className="center-align"
+              >
+                  <Button
+                    node='a'
+                    href='/login'
+                    waves="light"
+                  >
+                    Login
+                  </Button>
+              </Row>
+            </form>
+          </Card>
+        </Col>
+      </Row>
+    );
+  }
+}
+
+const validate = (values) => {
+  const errors = {};
+  if (!values.username) {
+    errors.username = 'Required';
+  }
+  if (!values.email) {
+    errors.email = 'Required';
+  }
+  if (!values.password) {
+    errors.password = 'Required';
+  } else if (values.password.length < 6) {
+    errors.password = 'Minimum of 6 characters';
+  }
+  return errors;
+};
+
+export default reduxForm({
+  form: 'signup',
+  validate,
+})(SignupForm);
