78d62583182ff6e390f623326469a176238a0c87
Victor Presumido <victor.presumido@gmail.com>
2018-03-08 16:23:57 -0300

    Load validateUserFields reducer on main reducer

--- web/src/reducers/index.js
+++ web/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import session from './session';
+import validateUserFields from './validateUserFields';
 
 const appReducer = combineReducers({
   form,
   session,
+  validateUserFields,
 });
 
 export default function (state, action) {
