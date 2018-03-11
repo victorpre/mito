f302daf18844bc5d06fc5d822ce1b41289417b18
Victor Presumido <victor.presumido@gmail.com>
2018-03-11 13:00:21 -0300

    Remove validateUserFields reducer

--- web/src/reducers/index.js
+++ web/src/reducers/index.js
@@ -1,12 +1,10 @@
 import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form';
 import session from './session';
-import validateUserFields from './validateUserFields';
 
 const appReducer = combineReducers({
   form,
   session,
-  validateUserFields,
 });
 
 export default function (state, action) {
