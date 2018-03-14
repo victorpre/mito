4aa333b647691161543cdc4612ab586d8f034c55
Victor Presumido <victor.presumido@gmail.com>
2018-03-14 20:24:05 -0300

    Start sockets Rooms channel

--- api/web/channels/room_channel.ex
+++ api/web/channels/room_channel.ex
@@ -1,3 +1,10 @@
 defmodule Mito.RoomChannel do
   use Mito.Web, :channel
+
+  def join("rooms:" <> room_id, %{"username" => username, "host" => host, "password" => pass}, socket) do
+    jid = "#{username}@#{host}"
+
+    {:ok, pid} = Mito.Auth.xmpp_login(jid, pass)
+
+  end
 end
